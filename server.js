3131ea4db9b4ae1690e9cad8405e5d5db44b7aa6
Soumen Biswas <45618379+soumenbiswas212@users.noreply.github.com>
2021-08-22 18:51:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Finally Ecr Complted\n')
+  response.end('Finally Ecr standard process\n')
 })
 
 server.listen(port)
